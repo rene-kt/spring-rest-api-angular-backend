b197ed4d5e7f185d5d88d4cc7be40be05ae3df80
Renê <renejr.arraes286@gmail.com>
2020-11-07 09:10:11 -0300

    inserting swagger

--- src/main/java/com/products/api/resources/ProductResource.java
+++ src/main/java/com/products/api/resources/ProductResource.java
@@ -3,6 +3,7 @@ package com.products.api.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,35 +16,45 @@ import org.springframework.web.bind.annotation.RestController;
 import com.products.api.models.Product;
 import com.products.api.repositories.ProductRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API REST PRODUCTS W/ SPRING BOOT")
+@CrossOrigin(origins = "*")
 public class ProductResource {
 	
 	@Autowired
 	ProductRepository productRepo;
 	
 	@GetMapping("/products")
+	@ApiOperation(value = "return all products from database")
 	public List<Product> listProducts(){
 		return productRepo.findAll();
 	}
 	
 	@GetMapping("/products/{id}")
+	@ApiOperation(value = "return one product from database by id")
 	public Product findProductById(@PathVariable(value = "id") long id){
 		return productRepo.findById(id);
 	}
 	
 	
 	@PostMapping("/product")
+	@ApiOperation(value = "insert one product into database")
 	public Product saveProduct(@RequestBody Product product) {
 		return productRepo.save(product);	
 	}
 	
 	@PutMapping("/product")
+	@ApiOperation(value = "update one product from database")
 	public Product editProduct(@RequestBody Product product) {
 		return productRepo.save(product);	
 	}
 	
 	@DeleteMapping("/product/{id}")
+	@ApiOperation(value = "delete one product from database by id")
 	public void deleteProductById(@PathVariable(value ="id") long id) {
 		productRepo.deleteById(id);	
 	}
