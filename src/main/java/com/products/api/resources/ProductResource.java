33fc6445a5c1e695450d5e39ec4c2722bd61b21f
Renê <renejr.arraes286@gmail.com>
2020-11-07 08:34:39 -0300

    put method

--- src/main/java/com/products/api/resources/ProductResource.java
+++ src/main/java/com/products/api/resources/ProductResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,12 @@ public class ProductResource {
 	public Product saveProduct(@RequestBody Product product) {
 		return productRepo.save(product);	
 	}
+	
+	@PutMapping("/product")
+	public Product editProduct(@RequestBody Product product) {
+		return productRepo.save(product);	
+	}
+	
 	@DeleteMapping("/product/{id}")
 	public void deleteProductById(@PathVariable(value ="id") long id) {
 		productRepo.deleteById(id);	
